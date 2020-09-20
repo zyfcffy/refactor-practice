d27f17636023117c709168d98b52f5cb1ed3d7f9
FuFangyuan <fangyuan.fu@thoughtworks.com>
2020-09-20 13:49:41 +0800

    [fufangyuan]finish exercise 4

--- 4/src/main/java/com/twu/refactoring/NumberCruncher.java
+++ 4/src/main/java/com/twu/refactoring/NumberCruncher.java
@@ -8,34 +8,18 @@ public class NumberCruncher {
     }
 
     public int countEven() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) count++;
-        }
-        return count;
+        return (int)Arrays.stream(numbers).filter(number -> number % 2 == 0).count();
     }
 
     public int countOdd() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number % 2 == 1) count++;
-        }
-        return count;
+        return (int)Arrays.stream(numbers).filter(number -> number % 2 == 1).count();
     }
 
     public int countPositive() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number >= 0) count++;
-        }
-        return count;
+        return (int)Arrays.stream(numbers).filter(number -> number >= 0).count();
     }
 
     public int countNegative() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number < 0) count++;
-        }
-        return count;
+        return (int)Arrays.stream(numbers).filter(number -> number < 0).count();
     }
 }
